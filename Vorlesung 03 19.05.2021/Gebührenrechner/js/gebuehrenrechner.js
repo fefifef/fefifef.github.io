7bc7fdc63106e12c4142b6ea3399876ef9442cb5
fefifef <steffen.kruschina@gmail.com>
2021-05-19 15:15:07 +0200

    add new task

---
+++ Vorlesung 03 19.05.2021/Gebührenrechner/js/gebuehrenrechner.js
@@ -0,0 +1,32 @@
+function berechnePreis(netto)
+{
+    anz = prompt();
+    let brutto = 0;
+    for(let i = 1; i <= anz; i++)
+    {
+        if(i <= 2)
+        {
+            brutto = brutto + netto * 1.19;
+        }
+        else
+        {
+            if(i <= 4)
+            {
+                brutto = brutto + (netto * 1.19 * 0.7);
+            }
+            else
+            {
+                if(i <= 7)
+                {
+                    brutto = brutto + (netto * 1.19 * 0.5);
+                }
+                else
+                {
+                    brutto = brutto + (netto * 1.19 * 0.4);
+                }
+            }
+        }
+    }
+    alert("Die Schulung kostet inkl. MwSt. (für " + anz + " Personen) "+ brutto.toFixed(2) + " Euro");
+}
+
