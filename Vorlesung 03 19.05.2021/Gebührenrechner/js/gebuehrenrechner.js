aa49a2d6fe7efe9d8abe39fdc17a1f096c3a809d
fefifef <steffen.kruschina@gmail.com>
2021-05-19 15:36:06 +0200

    Update gebuehrenrechner.js

--- Vorlesung 03 19.05.2021/Gebührenrechner/js/gebuehrenrechner.js
+++ Vorlesung 03 19.05.2021/Gebührenrechner/js/gebuehrenrechner.js
@@ -1,6 +1,6 @@
 function berechnePreis(netto)
 {
-    anz = prompt("Bitte gebe die Anzahl der Teilehmer ein");
+    let anz = prompt("Bitte gebe die Anzahl der Teilehmer ein");
     let brutto = 0;
     for(let i = 1; i <= anz; i++)
     {
