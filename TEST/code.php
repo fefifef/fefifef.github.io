cf4537f8999d7910278ba9d90610f39f098dc452
fefifef <steffen.kruschina@gmail.com>
2021-05-25 14:33:44 +0200

    TEST update

---
+++ TEST/code.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <style>
+            .item_left
+            {
+                grid-area: left; 
+                border: solid 1px black;
+                border-radius: 10px;
+                float: left;
+                width: 100%;
+            }
+            .item_right
+            {
+                grid-area: right; 
+                border: solid 1px black;
+                border-radius: 10px;
+                width: 100%;
+            }
+            .grid-container 
+            {
+                display: grid;
+                grid-template-areas:
+                    'left right';
+                grid-gap: 10px;
+            }
+            .grid-container > div 
+            {
+                background-color: rgba(255, 255, 255, 0.8);
+                padding: 20px 0;
+                font-size: 30px;
+            }
+        </style>
+    </head>
+    <body onload="sampleFunction()">
+        <script language="javascript" src="script.js"></script>
+        
+        <div class="grid-container">
+            <div class="item_left" id="block_left">
+            <?php
+                $zitate = file_get_contents('zitate.txt');
+                echo $zitate;
+            ?>
+            </div>
+            <div class="item_right" id="block_right">
+                
+            </div>
+        </div>
+    </body>
+</html>
