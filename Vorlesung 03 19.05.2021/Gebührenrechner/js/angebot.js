dc4d7a551fa0fb92b89dceb7c809c860da705566
fefifef <steffen.kruschina@gmail.com>
2021-05-26 14:11:42 +0200

    update

--- Vorlesung 03 19.05.2021/Gebührenrechner/js/angebot.js
+++ Vorlesung 03 19.05.2021/Gebührenrechner/js/angebot.js
@@ -1,11 +1,36 @@
+let wahl = 0;
+
 function onClickSonderpreis()
 {
     let p = document.getElementById("preis");
-
+    let g = document.getElementById("preisbereich");
     p.style.color = "red";
     p.style.fontSize = "16px";
-
-    let g = document.getElementById("preisbereich");
-
+  
     g.childNodes[0].style.color = "green";
+}
+
+
+function wertAnzeigen()
+{
+    let preis = document.getElementById("preis");
+    let txt = document.getElementById("preisbereich").childNodes[3];
+    let button = document.getElementById("preisbereich").childNodes[4];
+
+    if(wahl == 0)
+    {
+        preis.innerHTML = "<i>"+(preis.textContent / 1.19).toFixed(2)+"</i>";
+        preis.style.color = "grey";
+        txt.textContent = " Euro zzgl. 19% MwSt. ";
+        button.textContent = "zeige Bruttopreis";
+        wahl = 1;
+    }
+    else
+    {
+        preis.innerHTML = ""+(preis.textContent * 1.19).toFixed(2);
+        preis.style.color = "black";
+        txt.textContent = " Euro inkl. 19% MwSt. ";
+        button.textContent = "zeige Nettopreis";
+        wahl = 0;
+    }
 }
