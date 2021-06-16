891f6c477116d15aafd868b64d535e253dddc079
fefifef <steffen.kruschina@gmail.com>
2021-06-16 13:50:28 +0200

    ++

--- Vorlesung 03 19.05.2021/Gebührenrechner/js/bestellformular.js
+++ Vorlesung 03 19.05.2021/Gebührenrechner/js/bestellformular.js
@@ -4,24 +4,79 @@ function init()
 {
     document.bestellformular.anzahl.addEventListener("input", berechnePreis);
     document.bestellformular.waehrung.addEventListener("change", berechnePreis);
-    document.bestellformular.submit.addEventListener("submit",function(evt){return check(evt)});
+    document.bestellformular.lieferung_durch.addEventListener("change",changeInfo);
+    document.bestellformular.addEventListener("submit",function (e){return check(e)});
 }
 document.addEventListener("DOMContentLoaded", init);
 
 function check(evt)
 {
     let fehler = "";
+
+    if(document.bestellformular.firma.value == "")
+    {
+        fehler += "Sie müssen eine FIRMA angeben!\n";
+    }
+
     if(document.bestellformular.str.value == "")
     {
-        fehler += "Sie müssen eine Straße eingeben! \n";
+        fehler += "Sie müssen eine STRASSE angeben!\n";
     }
+
     if(document.bestellformular.plz.value.length < 4 || document.bestellformular.plz.value.length > 5)
     {
-        fehler += "Die PLZ muss aus 4 oder 5 zeichen bestehen!\n";
+        fehler += "Sie müssen eine PLZ mit min 4 und max 5 Zeichen angeben!\n";
     }
-    if(isNaN(document.bestellformular.plz.value))
+
+    if(isNaN(document.bestellformular.plz.value)){
+        fehler += "PLZ darf nur aus Zahlen bestehen.\n";
+    }
+    
+    if(document.bestellformular.ort.value == ""){
+        fehler += "Sie müssen einen ORT angeben!\n";
+    }
+
+    if(document.bestellformular.name.value == ""){
+        fehler += "Sie müssen einen NAMEN angeben!\n";
+    }
+
+    /*
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[09]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(document.bestellformular.email.value)){
+        fehler += "Dies scheint keine gültige E-Mail Adresse zu sein!\n";
+    }*/
+
+    if(document.bestellformular.abweichende_rechnungsadresse_select.value == "")
     {
-        fehler += "Die PLZ darf nur aus straßen bestehen";
+        fehler += "Wählen Sie eine Rechnungsadresse!\n";
+    }else
+    {
+        if(document.bestellformular.abweichende_rechnungsadresse_select.value == "abweichend")
+        {
+            if(document.bestellformular.rechnungsadresse_firma.value == ""){
+                fehler += "Sie müssen eine FIRMA angeben!\n";
+            }
+        
+            if(document.bestellformular.rechnungsadresse_str.value == ""){
+                fehler += "Sie müssen eine STRASSE angeben!\n";
+            }
+        
+            if(document.bestellformular.rechnungsadresse_plz.value.length < 4 || document.bestellformular.plz2.value.length > 5){
+                fehler += "Sie müssen eine PLZ mit min 4 und max 5 Zeichen angeben!\n";
+            }
+        
+            if(isNaN(document.bestellformular.rechnungsadresse_plz.value)){
+                fehler += "PLZ darf nur aus Zahlen bestehen.\n";
+            }
+            
+            if(document.bestellformular.rechnungsadresse_ort.value == ""){
+                fehler += "Sie müssen einen ORT angeben!\n";
+            }
+        }
+    }
+
+    if(document.bestellformular.agb.checked == false){
+        fehler += "Sie müssen die AGB's bestätigen!\n";
     }
 
     if(fehler != "")
@@ -30,6 +85,34 @@ function check(evt)
         evt.preventDefault();
     }
     return;
+}
+
+function handleChange1()
+{
+    document.getElementById("abweichende_rechnungsadresse").style.display = "none";
+}
+function handleChange2()
+{
+    document.getElementById("abweichende_rechnungsadresse").style.display = "block";
+}
+
+function changeInfo()
+{
+    if(document.bestellformular.lieferung_durch.value = "DHL")
+    {
+        document.getElementById("paketdienst").href="DHL.de"
+    }
+    else
+    {
+        if(document.bestellformular.lieferung_durch.value = "UPS")
+        {
+            document.getElementById("paketdienst").href="UPS.de"
+        }
+        else
+        {
+            document.getElementById("paketdienst").href="HERMES.de"   
+        }
+    }
 }
 
 function berechnePreis()
