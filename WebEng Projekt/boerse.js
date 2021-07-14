8fb07aa92da3965d671092bf236430c022a28379
fefifef <steffen.kruschina@gmail.com>
2021-07-14 15:02:39 +0200

    **

--- WebEng Projekt/boerse.js
+++ WebEng Projekt/boerse.js
@@ -1,9 +1,101 @@
-$( document ).ready(function() {
+'use strict';
+let canvas;
+let kontostand;
+let preis_aktie;
+let anz__aktien;
+let restzeit;
 
-    var c=document.getElementById("canvas");
-    var ctx=c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(95,50,40,0,2*Math.PI);
-    ctx.stroke();
+let button_start;
+let button_kauf;
+let button_verkauf_one;
+let button_verkauf_all;
 
-});
+let anzeige_kontostand;
+let anzeige_aktienKurs;
+let anzeige_anzAktien;
+let anzeige_restzeit;
+
+window.addEventListener("load", init);
+
+function init()
+{
+    restzeit = 300;
+    kontostand = 10000.00;
+    preis_aktie = 80.00;
+    anz__aktien = 0;
+    button_start = document.getElementById("button_game_start");
+    button_kauf = document.getElementById("button_buy_one");
+    button_verkauf_one = document.getElementById("button_sell_one");
+    button_verkauf_all = document.getElementById("button_sell_all");
+
+    button_start.addEventListener("click",start);
+    button_kauf.addEventListener("click",kauf_aktie);
+    button_verkauf_one.addEventListener("click",verkauf_aktie);
+    button_verkauf_all.addEventListener("click",verkauf_aktien);
+
+    button_kauf.disabled = true;
+    button_verkauf_one.disabled = true;
+    button_verkauf_all.disabled = true;
+
+    anzeige_kontostand = document.getElementById("kontostand");
+    anzeige_aktienKurs = document.getElementById("kurs_aktie");
+    anzeige_anzAktien  = document.getElementById("anz_aktien");
+    anzeige_restzeit   = document.getElementById("restzeit");
+
+    anzeige_kontostand.textContent = kontostand;
+    anzeige_aktienKurs.textContent = preis_aktie;
+    anzeige_anzAktien.textContent = anz__aktien;
+    anzeige_restzeit.textContent = restzeit;
+}
+
+function start()
+{
+    console.log("start Boersenspiel");
+    button_kauf.disabled = false;
+    button_verkauf_one.disabled = false;
+    button_verkauf_all.disabled = false;
+    button_start.disabled = true;
+}
+function kauf_aktie()
+{ 
+
+    if((kontostand - preis_aktie) > 0)
+    {
+        anz__aktien++;
+        kontostand = kontostand - preis_aktie;
+        anzeige_kontostand.textContent = kontostand;
+        anzeige_aktienKurs.textContent = preis_aktie;
+        anzeige_anzAktien.textContent = anz__aktien;
+    }
+}
+function verkauf_aktie()
+{
+    if(anz__aktien > 0)
+    {
+        anz__aktien--;
+        kontostand = kontostand+preis_aktie;
+        anzeige_kontostand.textContent = kontostand;
+        anzeige_aktienKurs.textContent = preis_aktie;
+        anzeige_anzAktien.textContent = anz__aktien;
+    }
+}
+
+function verkauf_aktien()
+{
+    if(anz__aktien > 0)
+    {
+        kontostand = kontostand + (preis_aktie * anz__aktien);
+        anz__aktien = 0;
+        anzeige_kontostand.textContent = kontostand;
+        anzeige_aktienKurs.textContent = preis_aktie;
+        anzeige_anzAktien.textContent = anz__aktien;
+    }
+}
+
+
+
+
+function drawCanvas()
+{
+    
+}
