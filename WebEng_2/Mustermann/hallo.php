0697a4e04b3a32cd3e4d5004829aedc439a6bea5
fefifef <steffen.kruschina@gmail.com>
2021-09-06 14:30:43 +0200

    Update hallo.php

--- WebEng_2/Mustermann/hallo.php
+++ WebEng_2/Mustermann/hallo.php
@@ -10,6 +10,16 @@
 			echo "Hallo <strong>Welt</strong>, es ist nun ";
 			echo date("d.m.Y H:i:s");
 			echo "Uhr!";
+
+
+			$farbe="rot";
+			echo "<p> Das Auto ist $farbe und schnell!</p>";
+
+			echo '<p> Das Auto ist $farbe und schnell!</p>';
+
+			echo "Ich habe ein " . $farbe . "es Auto <br/>";
+
+			echo "Ich habe ein {$farbe}es Auto <br/>";
 		?>
 		
 		<p> 
