3e7ee741d51715bde14eeadafd8799141968a070
fefifef <steffen.kruschina@gmail.com>
2021-09-06 14:01:59 +0200

    Create hallo.php

---
+++ WebEng_2/Mustermann/hallo.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Hallo PHP!</title>
+</head>
+
+<body>
+	<h1>Hallo Welt!</h1>
+	
+	<?php
+		echo "Hallo <strong>Welt</strong>, es ist nun ";
+		echo date("d.m.Y H:i:s");
+		echo "Uhr!";
+	?>
+	
+	<p> 
+		Das War's!
+	</p>
+	
+</body>
+</html>
