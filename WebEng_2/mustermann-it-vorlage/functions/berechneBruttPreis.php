9f97d1fe036530132e60680aefda7010fd25cfd8
fefifef <steffen.kruschina@gmail.com>
2021-09-27 14:20:54 +0200

    Update berechneBruttPreis.php

--- WebEng_2/mustermann-it-vorlage/functions/berechneBruttPreis.php
+++ WebEng_2/mustermann-it-vorlage/functions/berechneBruttPreis.php
@@ -1,14 +1,16 @@
-function berechneBruttPreis(float $einzelpreis) : float
-{
-    static $UmSt = 19;
-    $ergebnis = floatval($einzelpreis) * (1 + $UmSt/100);
-    if($UmSt == 19)
+<?php
+    function berechneBruttPreis(float $einzelpreis) : float
     {
-        $UmSt = $UmSt - 3;
-    }else
-    {
-        $UmSt = $UmSt - 3;
+        static $UmSt = 19;
+        $ergebnis = floatval($einzelpreis) * (1 + $UmSt/100);
+        if($UmSt == 19)
+        {
+            $UmSt = $UmSt - 3;
+        }else
+        {
+            $UmSt = $UmSt - 3;
+        }
+                
+        return $ergebnis;           
     }
-            
-    return $ergebnis;           
-}
+?>
