02823ceb9fbe35c7631f7b0e74b082141399ae8b
fefifef <steffen.kruschina@gmail.com>
2021-10-04 13:36:53 +0200

    Update feedback_zeigen.php

--- WebEng_2/mustermann-it-vorlage/feedback/feedback_zeigen.php
+++ WebEng_2/mustermann-it-vorlage/feedback/feedback_zeigen.php
@@ -54,7 +54,7 @@
       <div class="container-fluid py-5">
         
 		<h1 class="display-5 fw-bold">Kunden Feedback</h1>
-		
+
         <p class="col-md-9 fs-4">
           <?php
             $link = mysqli_connect("localhost","root","","webeng2_1")
@@ -63,15 +63,52 @@
             #---------- ---------- ---------- ---------- ----------#
             #echo"<br/><br/>";
             #---------- ---------- ---------- ---------- ----------#
-            $sql = "SELECT note,name,bewertung,name,mail,beitragsdatum FROM feedback";
-            $result = mysqli_query($link,$sql); 
-            while($row=mysqli_fetch_assoc($result))
-            {
-              echo "Am {$row['beitragsdatum']} von ({$row['name']}) <br/>";
-              echo "Bewertung: ({$row['note']}) \"{$row['bewertung']}\"";
-              echo "<br/><br/>";
-            }
-          ?>
+                      
+            echo "<button type='button' class='btn btn-primary' onclick=\"window.location.href = 'feedback_zeigen.php?wahl=0'\">Alle</button>  ";
+            echo "<button type='button' class='btn btn-primary' onclick=\"window.location.href = 'feedback_zeigen.php?wahl=1'\">Sterne 4 oder besser</button>  ";
+            echo "<button type='button' class='btn btn-primary' onclick=\"window.location.href = 'feedback_zeigen.php?wahl=2'\">Letzter Monat</button>  ";
+            echo "<br/>";
+
+              $wahl = 0;
+              if(isset($_GET['wahl']))
+              {
+                $wahl = $_GET['wahl'];
+              }
+              if($wahl == 0)
+              {
+                echo "<h1>Alle Feedbacks</h1>";
+                $sql = "SELECT note,name,bewertung,mail,beitragsdatum FROM feedback";
+              }else
+              {
+                if($wahl == 1)
+                {
+                  echo "<h1>Mit 4 oder mehr Sternen</h1>";
+                  $sql = "SELECT note,name,bewertung,mail,beitragsdatum FROM feedback WHERE note > 3";
+                }else
+                {
+                  echo "<h1>Letzer Monat</h1>";
+                  $sql = "SELECT note,name,bewertung,mail,beitragsdatum FROM feedback WHERE beitragsdatum >DATE_SUB(NOW(), INTERVAL 1 MONTH)";
+                }
+              }
+              $result = mysqli_query($link,$sql); 
+            
+              while($row=mysqli_fetch_assoc($result))
+              {
+                echo "Am {$row['beitragsdatum']} von {$row['name']} <br/>";
+                echo "Bewertung: ";
+                for($k = 0; $k < $row['note']; $k++)
+                {
+                  echo "<svg xmlns='http://www.w3.org/2000/svg' viewBox='-2 -2 24 24' width='24' height='24' preserveAspectRatio='xMinYMin' class='icon__icon'><path d='M10 16.207l-6.173 3.246 1.179-6.874L.01 7.71l6.902-1.003L10 .453l3.087 6.254 6.902 1.003-4.995 4.869 1.18 6.874z'></path></svg>";
+                }
+                for($k = 0; $k < (5 - $row['note']); $k++)
+                {
+                  echo "<svg xmlns='http://www.w3.org/2000/svg' viewBox='-2 -2.5 24 24' width='24' height='24' preserveAspectRatio='xMinYMin' class='icon__icon'><path d='M10 13.554l3.517 1.85-.672-3.917 2.846-2.774-3.932-.571L10 4.579 8.241 8.142l-3.932.571 2.846 2.774-.672 3.916L10 13.554zm0 2.26L3.827 19.06l1.179-6.875L.01 7.317l6.902-1.003L10 .06l3.087 6.254 6.902 1.003-4.995 4.868 1.18 6.875L10 15.814z'></path></svg>";
+                }
+                echo "({$row['note']}) \"{$row['bewertung']}\"";
+                echo "<br/><br/>";
+              }
+            
+            ?>
         </p>
 
         
